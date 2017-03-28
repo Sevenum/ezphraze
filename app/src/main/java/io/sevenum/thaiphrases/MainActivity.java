83641184ccaf6b7d6792a0c2513755c4a74514bb
Steve <steveharvey212@gmail.com>
2017-03-28 17:41:58 +0100

    Initial commit

---
+++ app/src/main/java/io/sevenum/thaiphrases/MainActivity.java
@@ -0,0 +1,30 @@
+package io.sevenum.thaiphrases;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void playSound(View view) {
+
+        int id = view.getId();
+        String ourId = "";
+
+        ourId = view.getResources().getResourceEntryName(id);
+
+        int resourceId = getResources().getIdentifier(ourId, "raw", "io.sevenum.thaiphrases");
+
+        MediaPlayer mplayer = MediaPlayer.create(this, resourceId);
+
+        mplayer.start();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
